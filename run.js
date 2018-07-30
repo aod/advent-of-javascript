061264586e9b2f13b2c18b7461d3eb4a4f4d2cd7
Oktay Dinler <24858380+aoktayd@users.noreply.github.com>
2018-07-30 21:59:27 +0200

    adds input for each day and modifies run.js to use new input method

--- run.js
+++ run.js
@@ -5,6 +5,11 @@ const path = require('path');
 
 const argv = require('yargs')
     .options({
+        year: {
+            alias: 'y',
+            type: 'number',
+            default: 2017
+        },
         day: {
             alias: 'd',
             type: 'number',
@@ -18,28 +23,22 @@ const argv = require('yargs')
         input: {
             alias: 'i',
             type: 'string',
-            default: 'run.input'
         }
     })
     .argv;
 
-if (argv.day > 0 && argv.day < 26) {
-    const dayNumber = argv.day < 10
-        ? `0${argv.day}`
-        : argv.day;
+const file = path.resolve(__dirname, `${argv.year}/day/${argv.day}/part${argv.part}.js`)
 
-    let file = path.join(__dirname, `day${dayNumber}-part${argv.part}.js`);
+if (fs.existsSync(file)) {
+    const puzzle = require(file);
+    let input = argv.input;
 
-    if (fs.existsSync(file)) {
-        const f = require(file);
+    input = argv.input
+        ? argv.input
+        : fs.readFileSync(
+            path.resolve(__dirname, `${argv.year}/day/${argv.day}/input`),
+            { encoding: 'UTF-8' }
+        );
 
-        let data = argv.input === undefined ? 'run.input' : argv.input;
-        let dataFile = path.join(__dirname, data);
-
-        if (fs.existsSync(dataFile)) {
-            data = fs.readFileSync(dataFile, { encoding: 'UTF-8' });
-        }
-
-        console.log(f(data));
-    }
+    console.log(puzzle(input));
 }
