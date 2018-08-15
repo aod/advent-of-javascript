46a31678ff53c5887b7c93e9d601f9e5b45660b5
Oktay Dinler <24858380+aoktayd@users.noreply.github.com>
2018-08-15 19:33:49 +0200

    completes day 10

--- 2017/day10.test.js
+++ 2017/day10.test.js
@@ -7,5 +7,10 @@ describe('day 10: Knot Hash', () => {
         expect(day10(testInput, 4)[0]).toBe(12)
     })
 
-    test('part 2', () => {})
+    test('part 2', () => {
+        expect(day10('')[1]).toBe('a2582a3a0e66e6e86e3812dcb672a272')
+        expect(day10('AoC 2017')[1]).toBe('33efeb34ea91902bb2f59c9920caa6cd')
+        expect(day10('1,2,3')[1]).toBe('3efbe78a8d82f29979031a4aa0b16a9d')
+        expect(day10('1,2,4')[1]).toBe('63960835bcdc130f0b66d7ff4f6a5a8e')
+    })
 })
--- 2017/day10/index.js
+++ 2017/day10/index.js
@@ -1,47 +1,94 @@
-const Process = require('../../lib/Process')
+const createArray = length => new Array(length + 1).fill(null).map((_, i) => i)
 
-const parse = function(input, length) {
-    let list = new Array(length + 1).fill(null).map((_, i) => i)
-    let position = 0
-    let skipSize = 0
-    let lengths = input.split(',').map(Number)
+const reverseByLength = ({ list, start, length }) => {
+    let listCopy = list.slice()
+
+    let part = {
+        ranges: [],
+        values: []
+    }
+
+    let tmpLength = length
+    while (tmpLength > 0) {
+        const index = (start + length - tmpLength) % list.length
+        part.ranges.push(index)
+        part.values.unshift(listCopy[index])
+        tmpLength--
+    }
+
+    part.values.forEach((v, i) => {
+        listCopy[part.ranges[i]] = v
+    })
+
+    return listCopy
+}
+
+const knot = ({ list, lengths, position, skipSize }) => {
+    let positionCopy = position
+    let skipSizeCopy = skipSize
+    let listCopy = list.slice()
 
     for (const length of lengths) {
         // Reverse the order of that length of elements in the list, starting
         // with the element at the current position.
-        let tmpLength = length
-        let part = {
-            ranges: [],
-            values: []
-        }
-
-        while (tmpLength > 0) {
-            const index = (position + length - tmpLength) % list.length
-            part.ranges.push(index)
-            part.values.unshift(list[index])
-            tmpLength--
-        }
-
-        part.values.forEach((v, i) => {
-            list[part.ranges[i]] = v
+        listCopy = reverseByLength({
+            list: listCopy,
+            start: positionCopy,
+            length
         })
 
         // Move the current position forward by that length plus the skip size.
-        position += (length + skipSize) % list.length
+        positionCopy = (positionCopy + length + skipSizeCopy) % listCopy.length
 
         // Increase the skip size by one
-        skipSize += 1
+        skipSizeCopy += 1
+    }
+    return { list: listCopy, position: positionCopy, skipSize: skipSizeCopy }
+}
+
+const part1 = (input, length = 255) => {
+    const { list } = knot({
+        list: createArray(length),
+        lengths: input.split(',').map(Number),
+        position: 0,
+        skipSize: 0
+    })
+
+    return list[0] * list[1]
+}
+
+const part2 = (input, length = 255) => {
+    let list = createArray(length)
+    let position = 0
+    let skipSize = 0
+    let lengths = input
+        .split('')
+        .map(_ => _.charCodeAt(0))
+        .concat(17, 31, 73, 47, 23)
+
+    for (let i = 0; i < 64; i++) {
+        const output = knot({
+            list,
+            lengths,
+            position,
+            skipSize
+        })
+        list = output.list
+        position = output.position
+        skipSize = output.skipSize
     }
 
-    this.fire('done', list)
+    let hashList = []
+    const totalSections = list.length / 16
+    while (hashList.length < totalSections) {
+        const part = list.splice(0, 16)
+        hashList.push(part)
+    }
+
+    return hashList
+        .map(arr => arr.reduce((acc, val) => (acc = acc ^ val), 0).toString(16))
+        .map(v => (v.length === 1 ? `0${v}` : v))
+        .join('')
 }
 
-module.exports = (input, length = 255) => {
-    const output = new Process(parse)
-        .on('done', function(list) {
-            this.list = list
-        })
-        .start(input, length)
-
-    return [output.list[0] * output.list[1], null]
-}
+module.exports = (...args) => [part1(...args), part2(...args)]
