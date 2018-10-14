84b53670d450f4b9939829d0919b4db818199e4b
Oktay <24858380+aoktayd@users.noreply.github.com>
2018-10-14 19:52:24 +0200

    overhauled benchmark.js

--- benchmark.js
+++ benchmark.js
@@ -2,26 +2,55 @@ const glob = require('glob')
 const path = require('path')
 const fs = require('fs')
 
-const aocFiles = glob.sync('20*/day*/+(part|input)!(*.test.js)')
-
-const inputs = new Map()
 const solutions = new Map()
 
-for (const aocFile of aocFiles) {
-    const [year, day, file] = aocFile.split(path.sep)
+for (const yearDir of glob.sync('20*/')) {
+    const year = path.basename(yearDir)
 
-    if (file === 'input') {
-        inputs.set(
-            `${year}/${day}`,
-            fs.readFileSync(aocFile, { encoding: 'UTF-8' })
+    const yearMap = new Map()
+    solutions.set(year, yearMap)
+
+    for (const dayDir of glob.sync(`${year}/day*`)) {
+        const day = path.basename(dayDir)
+
+        const dayMap = new Map()
+        dayMap.set(
+            'input',
+            fs.readFileSync(`${year}/${day}/input`, { encoding: 'UTF-8' })
         )
-    } else {
-        solutions.set(aocFile, require(`./${aocFile}`))
+        const daySolutions = new Map()
+        dayMap.set('solutions', daySolutions)
+        yearMap.set(day, dayMap)
+
+        for (const partFile of glob.sync(`${year}/${day}/part!(*.test).js`)) {
+            const part = path.parse(partFile).name
+
+            daySolutions.set(part, require(`./${partFile}`))
+        }
     }
 }
 
-for (const [file, solution] of solutions) {
-    console.time(file)
-    solution(inputs.get(path.dirname(file)))
-    console.timeEnd(file)
+for (const [year, days] of solutions) {
+    console.group(year)
+
+    for (const [day, parts] of days) {
+        console.group(day)
+
+        const input = parts.get('input')
+
+        for (const [part, solution] of parts.get('solutions')) {
+            const solve = solution.bind(null, input)
+
+            const start = process.hrtime()
+            solve()
+            const diff = process.hrtime(start)
+            const ms = diff[0] * 1e3 + diff[1] / 1e6
+
+            console.log(`solved ${part} in ${ms.toFixed(2)}ms`)
+        }
+
+        console.groupEnd()
+    }
+
+    console.groupEnd()
 }
