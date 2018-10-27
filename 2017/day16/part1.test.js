1717369c7849e5d9eda19a9e2b19e63f36d74379
Oktay <24858380+aoktayd@users.noreply.github.com>
2018-10-27 20:29:13 +0200

    adds 2017.16.1

--- 2017/day16/part1.test.js
+++ 2017/day16/part1.test.js
@@ -1,5 +1,8 @@
 const part1 = require('./part1')
 
 it('should pass the test input from day 16 part 1', () => {
-    // TODO
+    const input = 's1,x3/4,pe/b'
+    const programs = 5 // abcde
+
+    expect(part1(input, programs)).toBe('baedc')
 })
